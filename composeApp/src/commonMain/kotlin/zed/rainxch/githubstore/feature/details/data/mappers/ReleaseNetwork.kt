223d28de2f48e2c3904bb86f84fe85f4d018a457
Rainxch Zed <undefineduser087@gmail.com>
2025-12-23 23:02:20 +0500

    refactor: Extract AssetNetwork toDomain mapper
    Moved the `AssetNetwork.toDomain()` extension function from `ReleaseNetwork.kt` into its own dedicated file, `AssetNetwork.kt`. This improves code organization by separating the mapping logic for different data transfer objects.

--- composeApp/src/commonMain/kotlin/zed/rainxch/githubstore/feature/details/data/mappers/ReleaseNetwork.kt
+++ composeApp/src/commonMain/kotlin/zed/rainxch/githubstore/feature/details/data/mappers/ReleaseNetwork.kt
@@ -1,9 +1,7 @@
 package zed.rainxch.githubstore.feature.details.data.mappers
 
-import zed.rainxch.githubstore.core.domain.model.GithubAsset
 import zed.rainxch.githubstore.core.domain.model.GithubRelease
 import zed.rainxch.githubstore.core.domain.model.GithubUser
-import zed.rainxch.githubstore.feature.details.data.dto.AssetNetwork
 import zed.rainxch.githubstore.feature.details.data.dto.ReleaseNetwork
 
 fun ReleaseNetwork.toDomain(): GithubRelease = GithubRelease(
@@ -22,18 +20,4 @@ fun ReleaseNetwork.toDomain(): GithubRelease = GithubRelease(
     tarballUrl = tarballUrl,
     zipballUrl = zipballUrl,
     htmlUrl = htmlUrl
-)
-
-private fun AssetNetwork.toDomain(): GithubAsset = GithubAsset(
-    id = id,
-    name = name,
-    contentType = contentType,
-    size = size,
-    downloadUrl = downloadUrl,
-    uploader = GithubUser(
-        id = uploader.id,
-        login = uploader.login,
-        avatarUrl = uploader.avatarUrl,
-        htmlUrl = uploader.htmlUrl
-    )
 )
